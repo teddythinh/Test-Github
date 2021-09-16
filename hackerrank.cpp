26bc938a70e28ec05778a84ee51b68d7206667df
Teddy <cuongthinh2002@gmail.com>
2021-09-16 15:03:10 +0700

    update

--- hackerrank.cpp
+++ hackerrank.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 int main()
 {
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
-    if((c - b > d) || (c - a > d))
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n, x, flag = 1, start = 0;
+    cin >> n;
+    vector<int> a;
+    while (cin >> x)
     {
-        cout << "No";
+        a.push_back(x);
     }
-    else cout << "Yes";
+    for (int i = 1; i <= n; i++)
+    {
+        if (i != a[start])
+        {
+            flag = 0;
+            cout << i << endl;
+        }
+        else if (start + 1 == a.size() && a[start] != n)
+        {
+            for (int j = i; j <= n; j++)
+                cout << i << endl;
+            break;
+        }
+        else
+            start++;
+    }
+
+    if (flag == 1)
+        cout << "good job";
     return 0;
 }
