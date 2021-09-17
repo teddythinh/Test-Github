1068aab10e0ab29fdd5fd2a9930c619a27735268
Teddy <cuongthinh2002@gmail.com>
2021-09-18 00:25:05 +0700

    Update blah blah blah

--- hackerrank.cpp
+++ hackerrank.cpp
@@ -1,38 +1,20 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
+
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-
-    int n, x, flag = 1, start = 0;
+    int n, a[101];
+    float sum, avg, flag = 0;
     cin >> n;
-    vector<int> a;
-    while (cin >> x)
-    {
-        a.push_back(x);
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+        if(a[i] < 0) continue;
+        flag++;
+        sum += a[i];
+        
     }
-    for (int i = 1; i <= n; i++)
-    {
-        if (i != a[start])
-        {
-            flag = 0;
-            cout << i << endl;
-        }
-        else if (start + 1 == a.size() && a[start] != n)
-        {
-            for (int j = i; j <= n; j++)
-                cout << i << endl;
-            break;
-        }
-        else
-            start++;
-    }
-
-    if (flag == 1)
-        cout << "good job";
+    avg  = sum / flag;
+    cout << avg << endl;
     return 0;
 }
