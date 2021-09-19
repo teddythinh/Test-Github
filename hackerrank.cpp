acc8319cfeb7c4e0d419e577e7a3232b82aa637c
Teddy <cuongthinh2002@gmail.com>
2021-09-19 17:58:21 +0700

    Update

--- hackerrank.cpp
+++ hackerrank.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main()
 {
-    int n, a[101];
-    float sum, avg, flag = 0;
-    cin >> n;
-    for (int i = 0; i < n; i++){
-        cin >> a[i];
-        if(a[i] < 0) continue;
-        flag++;
-        sum += a[i];
-        
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    while (a != 1 || b != 2 || c != 3 || d != 4 || e != 5)
+    {
+        if (a > b)
+        {
+            swap(a, b);
+            cout << a << " " << b << " " << c << " " << d << " " << e << endl;
+        }
+        if (b > c)
+        {
+            swap(b, c);
+            cout << a << " " << b << " " << c << " " << d << " " << e << endl;
+        }
+        if (c > d)
+        {
+            swap(c, d);
+            cout << a << " " << b << " " << c << " " << d << " " << e << endl;
+        }
+        if (d > e)
+        {
+            swap(d, e);
+            cout << a << " " << b << " " << c << " " << d << " " << e << endl;
+        }
     }
-    avg  = sum / flag;
-    cout << avg << endl;
-    return 0;
 }
