1b0d291c885729679a28efc17cae1162cf79d913
Phạm Huy Cường Thịnh <85171754+teddythinh@users.noreply.github.com>
2021-09-19 23:44:42 +0700

    Update

--- hackerrank.cpp
+++ hackerrank.cpp
@@ -3,31 +3,5 @@
 using namespace std;
 int main()
 {
-    double c, n, a[1000], sum, count = 0, avg, ans;
-    cin >> c;
-
-    for(int i = 0; i < c; i++) {
-        cin >> n;
-        for(int j = 0; j < n; j++) {
-            cin >> a[j];
-            sum = sum + a[j];
-            avg = sum / n;
-            if(a[j] > avg) {
-                count++;
-            }
-            ans = (count / n) * 100;
-        }
-        for(int j = 0; j < n; j++) {
-            if(a[j] > avg) {
-                count++;
-            }
-            ans = (count / n) * 100;
-        }
-        cout << fixed << setprecision(3) << ans << "%" << endl;
-        sum = 0;
-        avg = 0;
-        count = 0;
-        ans = 0;
-    }
-    return 0;
+    cout << "thinh dep trai" << endl;
 }
