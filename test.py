6252872a32346aebf652475ad5f5f63bf55ae0e2
Teddy <cuongthinh2002@gmail.com>
2021-09-25 23:34:23 +0700

    update python

--- test.py
+++ test.py
@@ -1,17 +1,6 @@
-a = input()
-
-sub = 'pa','pe', 'pi', 'po', 'pu'
-if sub in a:
-    print(1, end=' ')
-if sub in b:
-    print(2, end=' ')
-if sub in c:
-    print(3, end=' ')
-if sub in d:
-    print(4, end=' ')
-if sub in e:
-    print(5, end=' ')
-if not sub in a and not sub in b and not sub in c and not sub in d and not sub in e:
-    print("HE GOT AWAY!")
+a = int(input())
+for i in range(a):
+    b = input()
+    
 
 
