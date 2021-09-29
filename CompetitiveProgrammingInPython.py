c9384d1129420e2d5181cc2227ca6db1a098411f
Teddy <cuongthinh2002@gmail.com>
2021-09-29 22:48:50 +0700

    Update python file

--- CompetitiveProgrammingInPython.py
+++ CompetitiveProgrammingInPython.py
@@ -1,6 +1,12 @@
-a = int(input())
-for i in range(a):
-    b = input()
-    
-
-
+b = "problem"
+n = 1001
+while n < 1001:
+    a = input()
+    if b in a:
+        print("yes")
+    else: print("no")
+    assert n > 0
+    chunk = fp.read(n)
+    if chunk == '':
+        break
+    process(chunk)
