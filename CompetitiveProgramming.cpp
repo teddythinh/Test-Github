73a4ae8b7a385ce845996fc77c34531171243072
Teddy <cuongthinh2002@gmail.com>
2021-10-03 00:21:26 +0700

    Updated

--- CompetitiveProgramming.cpp
+++ CompetitiveProgramming.cpp
@@ -2,21 +2,45 @@
 #include <string>
 #include <iomanip>
 #include <vector>
-#include <cmath> 
+#include <cmath>
 using namespace std;
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(nullptr);
+	vector<pair<string, int>> a, b;
+	 
 
-    int a, b;
-    cin >> a >> b;
-    int ans;
-    if(b % 2 == 0){
-        ans = ((b - a) + 1) / 2;
+//     string a;
+//     cin >> a;
+//     int count = 0;
+//     if(a == '_'){
+//         a[]
+//     }
+//     int c = stoi(a);
+//     if (c % 25 == 0)
+//     {
+//         count++;   
+//     }
+//     cout << count;
+    const int MOD = pow(10, 9) + 7;
+
+    long long n;
+    vector<int> a;
+    long long x;
+    cin >> n;
+    long long ans = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        a.push_back(x);
+        ans += (x * (i + 1)) % MOD;
     }
-    else {
-        ans = ((b - a) + 2) / 2;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            ans += ((long long)gcd(i + 1, j + 1) % MOD) * ((long long)gcd(a[i], a[j]) % MOD) * 2;
+        }
     }
     cout << ans;
-    return 0; 
+
+    return 0;
 }
--- CompetitiveProgrammingInPython.py
+++ CompetitiveProgrammingInPython.py
@@ -1,12 +1,10 @@
-print("Today's date?")
-date = input()
-print("Breakfast calories?")
-b = int(input())
-print("Lunch calories?")
-l = int(input())
-print("Dinner calories?")
-d = int(input())
-print("Snack calories?")
-s = int(input())
-sum = b + l + d + s
-print("calorie content for " + date + ": " + str(sum))
+a = input()
+b = "_"
+c = "X"
+if not b in a and not c in a:
+    a = int(a)
+if b in a:
+
+    b = '1' and '2' and '3' and '4 'and '5' and '6' and '7' and '8' and '9'
+if a % 25 == 0:
+    print(a / 25)
