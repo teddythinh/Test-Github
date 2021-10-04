749c3874c8cde19c3fc80fc9f0dbe09509d1efdf
Teddy <cuongthinh2002@gmail.com>
2021-10-04 20:35:34 +0700

    Updated

--- CompetitiveProgramming.cpp
+++ CompetitiveProgramming.cpp
@@ -1,46 +1,25 @@
 #include <iostream>
 #include <string>
-#include <iomanip>
-#include <vector>
-#include <cmath>
+#include <sstream>
 using namespace std;
 
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(nullptr);
-	vector<pair<string, int>> a, b;
-	 
-
-//     string a;
-//     cin >> a;
-//     int count = 0;
-//     if(a == '_'){
-//         a[]
-//     }
-//     int c = stoi(a);
-//     if (c % 25 == 0)
-//     {
-//         count++;   
-//     }
-//     cout << count;
-    const int MOD = pow(10, 9) + 7;
-
-    long long n;
-    vector<int> a;
-    long long x;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    string a, temp1, temp2;
     cin >> n;
-    long long ans = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> x;
-        a.push_back(x);
-        ans += (x * (i + 1)) % MOD;
-    }
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            ans += ((long long)gcd(i + 1, j + 1) % MOD) * ((long long)gcd(a[i], a[j]) % MOD) * 2;
+    while(n--){
+        cin >> a;
+        int ans;
+        if(a == "P=NP") cout << "skipped" << endl;
+        else{
+            stringstream ss(a);
+            getline(ss, temp1, '+');
+            getline(ss, temp2);
+            ans = stoi(temp1) + stoi(temp2);
+            cout << ans << endl;
         }
     }
-    cout << ans;
-
-    return 0;
 }
--- CompetitiveProgrammingInPython.py
+++ CompetitiveProgrammingInPython.py
@@ -1,10 +1,9 @@
-a = input()
-b = "_"
-c = "X"
-if not b in a and not c in a:
-    a = int(a)
-if b in a:
+a = int(input())
+for i in range(a):
+    b = input()
+    if b == "N=NP":
+        print("skipped")
+    else:
+        ans = int(b[0]) + int(b[2])
+        print(ans)
 
-    b = '1' and '2' and '3' and '4 'and '5' and '6' and '7' and '8' and '9'
-if a % 25 == 0:
-    print(a / 25)
