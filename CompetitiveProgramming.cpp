44ee1f82e897f73445f29ce4577f75345fe16b59
Teddy <cuongthinh2002@gmail.com>
2021-11-02 10:57:41 +0700

    Updated

--- CompetitiveProgramming.cpp
+++ CompetitiveProgramming.cpp
@@ -1,36 +1,15 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <sstream>
-
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-
-void fizzBuzz(int n) {
-    for(int i = 1; i <= n; i++) {
-        if (i % 5 == 0 && i % 3 == 0) cout << "FizzBuzz" << endl;
-        else if (i % 3 == 0) cout << "Fizz" << endl;
-        else if (i % 5 == 0) cout << "Buzz" << endl;
-        else cout << i << endl;
-    }
-}
-
-void ngu(int n) {
-    for(int i =  0; i < n; i++){
-    
-    }
-}
-
-int main()
+// Function to calculate triangle area with given base and height
+double triangleArea(double base, double height)
 {
-    int n;
-    cin >> n;
-    fizzBuzz(n);
+    return (base * height) / 2;
+}
 
+int main(){
+    double base, height;
+    cin >> base >> height;
+    cout << triangleArea(base, height) << endl;
     return 0;
 }
--- CompetitiveProgrammingInPython.py
+++ CompetitiveProgrammingInPython.py
@@ -1,8 +1,5 @@
-a = int(input())
-b = int(input())
-c = int(input())
-n = int(input())
-
-if a != 0 and b != 0 and c != 0 and (a + b + c) >= n and 3 <= n and n <= 20:
-    print("YES")
-else: print("NO")
+line = input()
+h, b = line.split()
+h = int(h)
+b = int(b)
+print((h*b)/2)
