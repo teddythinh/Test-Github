053750337cd2d3cdb4598ca5573b3b0a57e0ecc5
Teddy <cuongthinh2002@gmail.com>
2021-11-03 23:50:27 +0700

    Update a litle bit

--- CompetitiveProgramming.cpp
+++ CompetitiveProgramming.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <string>
+ 
 using namespace std;
-
-// Function to calculate triangle area with given base and height
-double triangleArea(double base, double height)
-{
-    return (base * height) / 2;
+ 
+// Complete the solve function below.
+void solve(vector<int> arr) {
+    int n = arr.size();
+    int max_sum = 0;
+    int max_index = 0;
+    int min_index = 0;
+    int min_sum = 0;
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+        if(sum > max_sum){
+            max_sum = sum;
+            max_index = i;
+        }
+        if(sum < min_sum){
+            min_sum = sum;
+            min_index = i;
+        }
+    }
+    cout << max_index + 1 << " " << min_index + 1 << endl;
 }
 
 int main(){
-    double base, height;
-    cin >> base >> height;
-    cout << triangleArea(base, height) << endl;
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       cin >> arr[arr_i];
+    }
+    solve(arr);
     return 0;
 }
--- CompetitiveProgrammingInPython.py
+++ CompetitiveProgrammingInPython.py
@@ -1,5 +1,21 @@
 line = input()
-h, b = line.split()
-h = int(h)
+a, b, c, d, e = line.split()
+a = int(a)
 b = int(b)
-print((h*b)/2)
+c = int(c)
+d = int(d)
+e = int(e)
+r = int(input())
+if r >= a:
+    print("A")
+elif r >= b:
+    print("B")
+elif r >= c:
+    print("C")
+elif r >= d:
+    print("D")
+elif r >= e:
+    print("E")
+else:
+    print("F")
+
