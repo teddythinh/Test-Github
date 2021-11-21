6564f65fa143e63479c13f2c5fbc8188f407670d
Teddy <cuongthinh2002@gmail.com>
2021-11-21 21:45:58 +0700

    Xam xam

--- CompetitiveProgramming.cpp
+++ CompetitiveProgramming.cpp
@@ -2,25 +2,32 @@
 #include <string>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    string s;
+int main()  {
     int n;
     cin >> n;
-    
-    for (int i = 0; i <= n; i++) {
-        getline(cin, s);
-        if (s.find("Simon says") != string::npos) {
-            for(int j = 10; j < s.size(); j++){
-                cout << s[j];
+    for(int i = 0; i < n;i++) {
+        int g;
+        cin >> g;
+        int a[g];
+        for(int j = 0; j < g; j++) {
+            cin >> a[j];
+        }
+        // print the element that is not repeated in the array
+        int flag = 0;
+        for(int j = 0; j < g; j++) {
+            for(int k = j+1; k < g; k++) {
+                if(a[j] == a[k]) {
+                    flag = 1;
+                    break;
+                }
             }
         }
-        else continue;
-        cout << endl;
+        if(flag == 0) {
+            cout << a[0] << endl;
+        }
+        
+        
     }
-
     return 0;
-
 
 }
--- CompetitiveProgrammingInPython.py
+++ CompetitiveProgrammingInPython.py
@@ -1,3 +1,9 @@
-a = int(input())
-b = int(input())
-print(a+b)
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+# find the number that is not in b
+for i in range(n):
+    if a[i] not in b:
+        print(a[i])
+        break
+
