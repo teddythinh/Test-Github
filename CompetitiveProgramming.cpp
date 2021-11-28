42352cd43ab9f89e895a352d96ff359c74f3db22
Teddy <cuongthinh2002@gmail.com>
2021-11-28 21:33:57 +0700

    ICPC training dayyyy

--- CompetitiveProgramming.cpp
+++ CompetitiveProgramming.cpp
@@ -1,50 +1,53 @@
 #include <iostream>
-#include <string>
-#include <sstream>
+#include <vector>
+#include <algorithm>
 #include <cmath>
+#include <string>
+#include <cstring>
+#include <sstream>
+
 using namespace std;
 
-typedef long long ll;
-
-int main()  {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        ll a, b;
-        cin >> a >> b;
-        if (b >= 3*a) cout << a << endl;
-        else if (a <= b && b < 3 * a) {
-            int x = 0;
-            x =
-        }
-
-
-        // if(a == b) {
-        //     cout << a/2 << endl;
-        // }
-        // else if(a == 0 || b == 0) cout << 0 << endl;
-        // else if(a == 4 || b == 4) cout << 0 << endl;
-        // else {
-        //     ll temp1 = (a<b)?a:b;
-        //     ll temp2 = a + b - temp1;
-        //     // temp1 *= 3;
-        //     int res = 0;
-        //     while(temp2 > temp1 && (temp1!=0 && temp2 != 0)) {
-        //          temp2 -= 3;
-        //          temp1 -= 1;
-        //          res+= 1;
-        //     }
-        //     while(temp1> temp2 && (temp1!=0 & temp2 != 0)) {
-        //         temp2 -= 1;
-        //         temp1 -= 3;
-        //         res += 1;
-        //     }
-        //     if(temp1 == temp2 && temp1 != 0) res+= temp1/2;
-        //     cout << res << endl;
-        // }
-    }
+long long revers(long long a) {
+    stringstream ss;
+    string temp;
+    ss << a;
+    temp = ss.str();
+    reverse(temp.begin(), temp.end());
+    return stoll(temp);
 }
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int a, b;
+    cin >> a >> b;
+    int res = 0;
+    for (int i = a; i < ceil(sqrt(a))*ceil(sqrt(a)); i++)
+    {
+        if(i % 10 != 0) {
+            long long sa = sqrt(i);
+            if(sa*sa == i) {
+                long long rev = revers(i);
+                long long sre = sqrt(rev);
+                if(sre * sre == rev) {
+                    res++;
+                }
+            }
+        }
+    }
+    for (long long i = ceil(sqrt(a)); i <= sqrt(b); i++)
+    {
+        if(i % 10 != 0 && i*i <= b) {
+            long long s = revers(i*i);
+            long long sa = sqrt(s);
+            if(sa*sa == s) {
+                res++;
+            }
+        }
+    }
+    cout << res << endl;
+}
+
+    
--- CompetitiveProgrammingInPython.py
+++ CompetitiveProgrammingInPython.py
@@ -1,5 +1,46 @@
-while True:
-    a = int(input())
-    print(a, flush=True)
-    if a == 42:
-        break
+# a = int(input())
+# b = int(input())
+
+# def isSquare(n):
+#     return int(n ** 0.5) ** 2 == n
+
+# def reverse(n):
+#     if n % 10 == 0:
+#         return 0
+#     return int(str(n)[::-1])
+
+# count = 0
+
+# for i in range(a, b + 1):
+#     temp = reverse(i)
+#     if temp % 10 == 0:
+#         continue
+#     if isSquare(i) and isSquare(reverse(i)):
+#         count += 1
+# print(str(count))
+
+n = int(input())
+count = 0
+for i in range(n):
+    a = str(input())
+    t1 = "a"
+    t2 = "e"
+    t3 = "i"
+    t4 = "o"
+    t5 = "u"
+    t6 = "ea"
+    if t1 in a:
+        count += 1
+    if t2 in a:
+        count += 1
+    if t3 in a:
+        count += 1
+    if t4 in a:
+        count += 1
+    if t5 in a:
+        count += 1
+    print(count)
+    count = 0
+
+
+   
