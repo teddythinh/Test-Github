73c327e422b086c8d900415d3f70972708c34d6c
Teddy <cuongthinh2002@gmail.com>
2022-01-04 22:48:35 +0700

    Some competitive programming tool updated

--- CompetitiveProgramming.cpp
+++ CompetitiveProgramming.cpp
@@ -7,10 +7,13 @@
 #include <fstream>
 #include <algorithm>
 #include <cmath>
-
+ 
 using namespace std;
-
-void solve() {
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
     int t;
     cin >> t;
     while (t--) {
@@ -45,45 +48,5 @@ void solve() {
             }
         }
     }
-}
-
-void solveB() {
-    int t;
-    cin >> t;
-    int left, right, coin;
-    vector<int> l, r, c;
-    while(t--) {
-        int n;
-        cin >> n;
-        int total = 0;
-        for (int i = 0; i < n; i++) {
-            cin >> left >> right >> coin;
-            l.push_back(left);
-            r.push_back(right);
-            c.push_back(coin);
-        }
-        for(int i = 0; i < n; i++) {
-            total += c[i];
-            //cout << c[i] << " ";
-            cout << total << "\n";
-        }
-        total = 0;
-    }
-    // while(t--) {
-    //     int total = 0;
-    //     for(int i = 0; i < c.size(); i++) {
-    //         total += c[i];
-    //     }
-    //     cout << total << "\n";
-    //     total = 0;
-    // } 
-}
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    solveB();
-    
     return 0;
 }
